5501afa91aaf513b25b22860992cc81470870746
Глеб Трофимов <GetromGleb@gmail.com>
2025-06-25 23:18:38 +0300

    update on teacher comments

--- src/widget.py
+++ src/widget.py
@@ -7,12 +7,12 @@ def mask_account_card(card_details: str) -> str:
     mask_card = get_mask_card_number(card_details[-16:])
 
     if card_details[0:4] == "Счет":
-        return card_details[0:5] + mask_account
+        return f"{card_details[0:5]}{mask_account}"
     else:
-        return card_details[:-16] + mask_card
+        return f"{card_details[:-16]}{mask_card}"
 
 
 def get_date(unformatted_date: str) -> str:
     """возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
-    formatted_date = unformatted_date[8:10] + unformatted_date[4:8] + unformatted_date[0:4]
-    return formatted_date.replace("-", ".")
+    return (f"{unformatted_date[8:10]}{unformatted_date[4:8]}{unformatted_date[0:4]}"
+            .replace("-", "."))
--- tests/test_2.py
+++ tests/test_2.py
@@ -10,21 +10,23 @@ def mask_account_card(card_details):
     #account_number = card_details[-20:]
 
     if card_details[0:4] == "Счет":
-        return card_details[0:5] + mask_account
+        return f"{card_details[0:5]}{mask_account}"
     else:
-        return card_details[:-16] + mask_card
+        return f"{card_details[:-16]}{mask_card}"
 
 
 
-print(mask_account_card("Visa Platinum 8990922113665229"))
+print(mask_account_card("Счет 73654108430135874305"))
 
 
 
 def get_date(unformatted_date: str) -> str:
-    formatted_date = unformatted_date[8:10] + unformatted_date[4:8] + unformatted_date[0:4]
+    #formatted_date = unformatted_date[8:10] + unformatted_date[4:8] + unformatted_date[0:4]
     #formatted_date = ".".join(unformatted_date[8:10], unformatted_date[5:7], unformatted_date[0:4])
     #formatted_date = unformatted_date[0:10]
-    return formatted_date.replace("-",".")
+    #return formatted_date.replace("-",".")
+    return (f"{unformatted_date[8:10]}{unformatted_date[4:8]}{unformatted_date[0:4]}"
+            .replace("-","."))
 
 
 print(get_date("2024-03-11T02:26:18.671407"))
