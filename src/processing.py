0d57e52c58f0a1a01e58647dc92409e7bbb31055
Глеб Трофимов <GetromGleb@gmail.com>
2025-07-03 23:02:25 +0300

    PEP8 filtering

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(bill_details: dict, state='EXECUTED') -> dict:
+def filter_by_state(bill_details: dict, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и возвращает списки опционально значения для ключа state"""
-    return [i for i in bill_details if i['state'] == state]
+    return [i for i in bill_details if i["state"] == state]
 
 
-def sort_by_date(bill_details: dict, date_sort=True) -> dict:
+def sort_by_date(bill_details: dict, date_sort: bool = True) -> list:
     """функция принимает список словарей и возвращает его в заданном порядке по дате"""
-    return sorted(bill_details, key=lambda d: d['date'], reverse=date_sort)
+    return sorted(bill_details, key=lambda d: d["date"], reverse=date_sort)
