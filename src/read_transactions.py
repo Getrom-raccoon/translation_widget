ae0171dc00d276e5b1accae8b467048e9782c0c2
Глеб Трофимов <GetromGleb@gmail.com>
2025-09-01 19:36:30 +0300

    add read_transactions.py and test

---
+++ src/read_transactions.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+def read_csv_transactions(file_patch):
+    """
+    Функция считывает финансовые операции из CSV-файла и возвращает список словарей.
+    :param file_patch: Путь к CSV-файлу
+    :return: список словарей
+    """
+    df = pd.read_csv(file_patch)
+    return df.to_dict(orient='records')
+
+def read_excel_transactions(file_patch):
+    """
+    Функция считывает финансовые операции из EXCEL-файла и возвращает список словарей.
+    :param file_patch: Путь к EXCEL-файлу
+    :return: список словарей
+    """
+    df = pd.read_excel(file_patch)
+    return df.to_dict(orient='records')
